e32e1e634888f8997658c89f3f78d63771aaced3
CodeAadarsh <aadarsh24lv@gmail.com>
2023-09-28 22:12:50 +0530

    new learning proj.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { serverComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
+import { InputFormsComponent } from './input-forms/input-forms.component'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    serverComponent,
+    ServersComponent,
+    InputFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
